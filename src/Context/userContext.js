f0b8503e39b13b9bf630c80cbb36ccdbf710d303
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2022-10-14 15:10:14 -0400

    useReducer

--- src/Context/userContext.js
+++ src/Context/userContext.js
@@ -1,20 +1,38 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+
+const SIGN_IN = 'sign-in';
+const SIGN_OUT = 'sign-out';
 
 export const userContext = createContext();
 
-const UserContextProvider = ({children}) => {
+// Reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SIGN_IN: {
+      return { ...state, user: { ...action.payload.userData } };
+    }
+    case SIGN_OUT: {
+      return undefined;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+const UserContextProvider = (props) => {
+  const { children } = props;
   const userInitialState = undefined;
 
-  const [user, setUser] = useState(userInitialState);
-
-  const signIn = (userData) => setUser(userData);
-
-  const signOut = () => setUser(userInitialState);
+  const [user, dispatch] = useReducer(reducer, userInitialState);
+  const signIn = (userData) => dispatch({ type: SIGN_IN, payload: { userData } });
+  const signOut = () => dispatch({ type: SIGN_OUT });
     
     return(
-        <userContext.Provider value={{ user, signIn, signOut }}>
-            {children}
-        </userContext.Provider>
+      <userContext.Provider value={{ user, signIn, signOut }}>
+        {children}
+      </userContext.Provider>
       )
 }
 
